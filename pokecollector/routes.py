43ac6f1fc6cfbd60782fbe311e75718f821a9f13
Stephen Iles <stephenciles@gmail.com>
2024-07-30 21:16:39 +0100

    Fixed issue with None types when looking at trainer cards

--- pokecollector/routes.py
+++ pokecollector/routes.py
@@ -46,58 +46,60 @@ def cards(card_id):
     retreat_urls = []
     
     # checks for pokemon types and assigns the correct image
-    match card[0].types:
-        case ['Water']:
-            type_url = 'img/water.png'
-        case ['Colorless']:
-            type_url = 'img/colorless.png'
-        case ['Darkness']:
-            type_url = 'img/darkness.png'
-        case ['Dragon']:
-            type_url = 'img/dragon.png'
-        case ['Fairy']:
-            type_url = 'img/fairy.png'
-        case ['Fighting']:
-            type_url = 'img/fighting.png'
-        case ['Fire']:
-            type_url = 'img/fire.png'
-        case ['Grass']:
-            type_url = 'img/grass.png'
-        case ['Lightning']:
-            type_url = 'img/lightning.png'
-        case ['Metal']:
-            type_url = 'img/metal.png'
-        case ['Psychic']:
-            type_url = 'img/psychic.png'
+    if card[0].types != None:
+        match card[0].types:
+            case ['Water']:
+                type_url = 'img/water.png'
+            case ['Colorless']:
+                type_url = 'img/colorless.png'
+            case ['Darkness']:
+                type_url = 'img/darkness.png'
+            case ['Dragon']:
+                type_url = 'img/dragon.png'
+            case ['Fairy']:
+                type_url = 'img/fairy.png'
+            case ['Fighting']:
+                type_url = 'img/fighting.png'
+            case ['Fire']:
+                type_url = 'img/fire.png'
+            case ['Grass']:
+                type_url = 'img/grass.png'
+            case ['Lightning']:
+                type_url = 'img/lightning.png'
+            case ['Metal']:
+                type_url = 'img/metal.png'
+            case ['Psychic']:
+                type_url = 'img/psychic.png'
                     
-    # checks for attack type, adding images for each attck cost.        
-    for attack in card[0].attacks:
-        for each in attack.cost:
-            attack_cost_urls = []
-            match each:
-                case 'Water':
-                    attack_cost_urls.append('img/water.png')
-                case 'Colorless':
-                    attack_cost_urls.append('img/colorless.png')
-                case 'Darkness':
-                    attack_cost_urls.append('img/darkness.png')
-                case 'Dragon':
-                    attack_cost_urls.append('img/dragon.png')
-                case 'Fairy':
-                    attack_cost_urls.append('img/fairy.png')
-                case 'Fighting':
-                    attack_cost_urls.append('img/fighting.png')
-                case 'Fire':
-                    attack_cost_urls.append('img/fire.png')
-                case 'Grass':
-                    attack_cost_urls.append('img/grass.png')
-                case 'Lightning':
-                    attack_cost_urls.append('img/lightning.png')
-                case 'Metal':
-                    attack_cost_urls.append('img/metal.png')
-                case 'Psychic':
-                    attack_cost_urls.append('img/psychic.png')
-            attacks_urls.append(', '.join(attack_cost_urls))  # converts list to string.  
+    # checks for attack type, adding images for each attck cost.       
+    if card[0].attacks != None: 
+        for attack in card[0].attacks:
+            for each in attack.cost:
+                attack_cost_urls = []
+                match each:
+                    case 'Water':
+                        attack_cost_urls.append('img/water.png')
+                    case 'Colorless':
+                        attack_cost_urls.append('img/colorless.png')
+                    case 'Darkness':
+                        attack_cost_urls.append('img/darkness.png')
+                    case 'Dragon':
+                        attack_cost_urls.append('img/dragon.png')
+                    case 'Fairy':
+                        attack_cost_urls.append('img/fairy.png')
+                    case 'Fighting':
+                        attack_cost_urls.append('img/fighting.png')
+                    case 'Fire':
+                        attack_cost_urls.append('img/fire.png')
+                    case 'Grass':
+                        attack_cost_urls.append('img/grass.png')
+                    case 'Lightning':
+                        attack_cost_urls.append('img/lightning.png')
+                    case 'Metal':
+                        attack_cost_urls.append('img/metal.png')
+                    case 'Psychic':
+                        attack_cost_urls.append('img/psychic.png')
+                attacks_urls.append(', '.join(attack_cost_urls))  # converts list to string.  
         
     # check the weakeness type and assigns the correct url
     if card[0].weaknesses != None:   
@@ -129,32 +131,33 @@ def cards(card_id):
             weakness_urls.append(', '.join(each_weakness_urls))  # converts list to string.
             
     # checks the retreat cost and assigns the correct urls
-    for retreat in card[0].retreatCost:
-        each_retreat_urls = []
-        match retreat:
-            case 'Water':
-                each_retreat_urls.append('img/water.png')
-            case 'Colorless':
-                each_retreat_urls.append('img/colorless.png')
-            case 'Darkness':
-                each_retreat_urls.append('img/darkness.png')
-            case 'Dragon':
-                each_retreat_urls.append('img/dragon.png')
-            case 'Fairy':
-                each_retreat_urls.append('img/fairy.png')
-            case 'Fighting':
-                each_retreat_urls.append('img/fighting.png')
-            case 'Fire':
-                each_retreat_urls.append('img/fire.png')
-            case 'Grass':
-                each_retreat_urls.append('img/grass.png')
-            case 'Lightning':
-                each_retreat_urls.append('img/lightning.png')
-            case 'Metal':
-                each_retreat_urls.append('img/metal.png')
-            case 'Psychic':
-                each_retreat_urls.append('img/psychic.png')
-        retreat_urls.append(', '.join(each_retreat_urls))  # converts list to string.      
+    if card[0].retreatCost != None:
+        for retreat in card[0].retreatCost:
+            each_retreat_urls = []
+            match retreat:
+                case 'Water':
+                    each_retreat_urls.append('img/water.png')
+                case 'Colorless':
+                    each_retreat_urls.append('img/colorless.png')
+                case 'Darkness':
+                    each_retreat_urls.append('img/darkness.png')
+                case 'Dragon':
+                    each_retreat_urls.append('img/dragon.png')
+                case 'Fairy':
+                    each_retreat_urls.append('img/fairy.png')
+                case 'Fighting':
+                    each_retreat_urls.append('img/fighting.png')
+                case 'Fire':
+                    each_retreat_urls.append('img/fire.png')
+                case 'Grass':
+                    each_retreat_urls.append('img/grass.png')
+                case 'Lightning':
+                    each_retreat_urls.append('img/lightning.png')
+                case 'Metal':
+                    each_retreat_urls.append('img/metal.png')
+                case 'Psychic':
+                    each_retreat_urls.append('img/psychic.png')
+            retreat_urls.append(', '.join(each_retreat_urls))  # converts list to string.      
                 
             
     return render_template("card_page.html", card=card, subtypes=subtypes, type_url=type_url, attacks_urls=attacks_urls, weakness_urls=weakness_urls, retreat_urls=retreat_urls)
