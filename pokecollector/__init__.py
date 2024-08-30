ed84039a139ba06339b35670a650692d1e8eb0f1
Stephen Iles <stephenciles@gmail.com>
2024-08-30 13:31:17 +0100

    Push another init fix

--- pokecollector/__init__.py
+++ pokecollector/__init__.py
@@ -12,8 +12,7 @@ app.config["SECRET_KEY"] = os.environ.get("SECRET_KEY")
 if os.environ.get("DEVELOPMENT") == "True":
     app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DB_URL")
 else:
-    uri = os.environ.get("DATABASE_URL")
-    app.config["SQLALCHEMY_DATABASE_URI"] = uri
+    app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DB_URL")
 
 db = SQLAlchemy(app)
 
